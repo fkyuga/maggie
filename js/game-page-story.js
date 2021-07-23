97579cb7365d0fd62e38295a195f233b2c40fc4f
Faiz Saleem <hi@fzslm.me>
2021-07-24 00:07:51 +0100

    maggie wake up and whees off the screen

--- js/game-page-story.js
+++ js/game-page-story.js
@@ -14,14 +14,12 @@ game.pages.story = {
                            animations. */
                         game.pages.story.scenes[1].animate();
                     } });
-                    tl2.timeScale(10);
 
                     $('.scene1').addClass('scene--active');
                     tl2.to('.scene1', .000001, { opacity: 0, scale: 6 })
                     tl2.to('.scene0', 2, { scale: 0.5, opacity: .5 });
                     tl2.to('.scene1', 2, { scale: 1, opacity: 1}, '-=2')
                 } });
-                tl.timeScale(10);
                 tl.to('.city', 7, { x: 37, ease:Linear.easeNone } );
                 tl.to('.car', 3, { x: 2000 }, '-=5')
             }
@@ -61,6 +59,42 @@ game.pages.story = {
                     sleepBubblesTl.to('.sleepbubbles *', .00001, { opacity: 0 })
                     sleepBubblesTl.to('.sleepbubble3', .00001, { opacity: 1 })
                     sleepBubblesTl.to('.sleepbubble3', .5, { opacity: 1 })
+
+                /* Play alarm clock sound after 2s */
+                setTimeout(function(){
+                    game.sfx.play('alarm');
+                    setTimeout(function(){
+                        /* Wake maggie up (stop her sleeping anim) */
+                        $('.scene2 .maggie-pajamas').addClass('awake');
+                        $('.scene2 .sleepbubbles').hide();
+                        $('.scene2 .maggie-pajamas .expression-sleeping').hide();
+                        $('.scene2 .maggie-pajamas .expression-shocked').show();
+
+                        game.sfx.play('magnet')
+                        gsap.to('.scene2 .clock', .2, {
+                            left: 562,
+                            rotate: 359,
+                            onComplete: function(){
+                                setTimeout(function(){
+                                    $('.scene2 .maggie-pajamas .expression-shocked').hide();
+                                    $('.scene2 .maggie-pajamas .expression-screaming').show();
+                                }, 1000)
+                                setTimeout(function(){
+                                    $('.scene2 .maggie-pajamas .expression-sleeping').hide();
+
+                                    gsap.to('.scene2 .maggie-pajamas, .scene2 .clock', .1, {
+                                        x: -800
+                                    })
+                                    game.sfx.play('whee');
+                                }, 2000)
+                            }
+                        })
+                    }, 1000)
+                }, 2000)
+
+                /* Alarm clock magnetically flies onto Maggie, shocking
+                   her out of sleep and waking her up */
+                
             }
         }
     ]
